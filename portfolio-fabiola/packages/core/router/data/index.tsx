b55f9efc6f78e42f398d352035ef0ad7a653e488
Fabiola Poblete <fabiola.broccolo@gmail.com>
2024-01-19 10:02:31 +0100

    Project setup with packages

---
+++ portfolio-fabiola/packages/core/router/data/index.tsx
@@ -0,0 +1,32 @@
+// import {
+//     createBrowserRouter,
+//     RouterProvider,
+//   } from "react-router-dom";
+  
+// import { LandingPage } from "@zocom/landing-page";
+// import { ConfirmationPage } from "@zocom/confirmation-page";
+// import { KitchenPage } from "@zocom/kitchen-page";
+
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <LandingPage />,
+//   },
+//   {
+//     path: '/kitchen',
+//     element: <KitchenPage />
+//   },
+//   {
+//     path: '/order/:orderNr',
+//     element: <ConfirmationPage />
+//   },
+//   {
+//     path: '*',
+//     element: <p>Page Not Found</p>,
+//   },
+// ]);
+
+// export const AppRoutes = () => {
+//   return <RouterProvider router={router} />;
+// };
