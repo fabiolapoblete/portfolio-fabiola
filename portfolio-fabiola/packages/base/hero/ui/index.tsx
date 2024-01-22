746a639e115946b1efe1f3758c6027de4dd08ab7
Fabiola Poblete <fabiola.broccolo@gmail.com>
2024-01-22 22:14:48 +0100

    Styling for hero done

--- portfolio-fabiola/packages/base/hero/ui/index.tsx
+++ portfolio-fabiola/packages/base/hero/ui/index.tsx
@@ -1,3 +1,5 @@
+import './style.scss'
+
 export const Hero = () => {
     return (
         <section className='hero' id='hero'>
@@ -6,7 +8,7 @@ export const Hero = () => {
             src='../../public/assets/profil-fabiola.jpeg'
             alt="Profile picture of Fabiola Poblete"
             />
-        <section>
+        <section className='hero__heading'>
             <h1>Hello, i'm <span>Fabiola</span></h1>
             <h3>frontend developer</h3>
         </section>
