0c8157c231331cc2b2023181ddbe1fd1f8cc2566
Fabiola Poblete <fabiola.broccolo@gmail.com>
2024-01-25 14:47:57 +0100

    fix for hamburger menu and styling

--- packages/base/hamburgerMenu/ui/index.tsx
+++ packages/base/hamburgerMenu/ui/index.tsx
@@ -1,65 +1,51 @@
-import { useState } from "react"
-import { HamburgerIcon, CloseIcon } from '../../../core/assets/svgIcons'
-import { Link } from 'react-scroll'
 import './style.scss';
 
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { HamburgerIcon, CloseIcon } from '../../../core/assets/svgIcons';
+
 export const HamburgerMenu = () => {
-    const navItems = [
-        {
-            name: 'Home',
-            path: 'hero'
-        }, 
-        {
-            name: 'About me',
-            path: 'about'
-        },
-        {
-            name: 'Skills',
-            path: 'skills'
-        }, 
-        {
-            name: 'Projects',
-            path: 'projects'
-        }, 
-        {
-            name: 'Contact',
-            path: 'contact'
-        }, 
-    ]
+  const navItems = [
+    {
+      name: 'Home',
+      path: '/',
+    },
+    {
+      name: 'About me',
+      path: '/about',
+    },
+    {
+      name: 'Projects',
+      path: '/projects',
+    },
+    {
+      name: 'Contact',
+      path: '/contact',
+    },
+  ];
 
-    const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
-    const toggleMenu = () => {
-        setIsOpen(!isOpen)
-    }
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return (
-        <nav className={`hamburgerMenu ${isOpen ? "open" : ''}`}>
-            <div className="hamburgerMenu__icons" onClick={toggleMenu}>
-                {isOpen ? CloseIcon : HamburgerIcon}
-            </div>
-            {
-                isOpen && (
-                    <ul className="hamburgerMenu__links">
-                        {
-                            navItems.map((item) => (
-                                <li key={item.name}>
-                                    <Link 
-                                    onClick={toggleMenu} 
-                                    to={item.path}
-                                    spy={true}
-                                    smooth={true}
-                                    offset={-70}
-                                    duration={900}
-                                    >
-                                        <p>{item.name}</p>
-                                    </Link>
-                                </li>
-                            ))
-                        }
-                    </ul>
-                )
-            }
-        </nav>
-    )
-}
+  return (
+    <nav className={`hamburgerMenu ${isOpen ? 'open' : ''}`}>
+      <div className='hamburgerMenu__icons' onClick={toggleMenu}>
+        {isOpen ? CloseIcon : HamburgerIcon}
+      </div>
+      {isOpen && (
+        <ul className='hamburgerMenu__links'>
+          {navItems.map((item) => (
+            <li key={item.name}>
+              <Link to={item.path}>
+                <p>{item.name}</p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </nav>
+  );
+};
